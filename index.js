fdbe1a7db3032c73a1b0ec500a14416522c1f0a9
Kingfuse <66550030+Kingfuse@users.noreply.github.com>
2022-04-24 18:09:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = function noMoreDelayedBuffs(mod) {
+	if (mod.majorPatchVersion < 97) {
+		mod.log("This mod fixing issues with x64 client only");
+		return;
+	}
+
+	mod.game.initialize("me");
+	
+	mod.hook("S_ABNORMALITY_REFRESH", 2, { "order": 99999999, "filter": { "fake": null } }, (event) => {
+		if (event.target !== mod.game.me.gameId || event.stacks < 2) return;
+
+		mod.send("S_ABNORMALITY_END", 1, {
+			"target": mod.game.me.gameId,
+			"id": event.id
+		});
+		mod.send("S_ABNORMALITY_BEGIN", mod.majorPatchVersion <= 106 ? 4 : 5, {
+			"target": mod.game.me.gameId,
+			"source": mod.game.me.gameId,
+			"id": event.id,
+			"duration": event.duration,
+			"stacks": event.stacks
+		});
+		return false;
+	});
+};
